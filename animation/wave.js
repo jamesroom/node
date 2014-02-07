2bc113c768f3dcb3c60fa5db832cc3276b737b8c
江仑 <lunjiang@anjuke.com>
2014-02-07 19:12:05 +0800

    ok

--- animation/wave.js
+++ animation/wave.js
@@ -1,3 +1,21 @@
 /**
  * Created by kathleen on 14-1-25.
  */
+J={
+    /**
+     * 支持传多个参数
+     * @param fun
+     * @param delay
+     */
+    
+    setTimeout:function(fun,delay){
+	var type = Object.prototype.toString.call(fun);
+	if(type == '[object Function]'){
+	    var argu = Array.prototype.slice(2,arguments);
+	    var f= function(){
+		    fun.apply(null,argu);
+		}
+	   return setTimeout(f,delay);
+	}	
+    }
+}
