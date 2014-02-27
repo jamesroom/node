040295e8076b300a45492a61367e3da2e9a5d5a7
jamesroom@163.com <jamesroom@163.com>
2014-02-28 01:03:34 +0800

    over

---
+++ Stepify/Util.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lunjiang on 14-2-27.
+ */
+var Util = {
+    isFunction:function(fn){
+        return  Object.prototype.toString.call(fn) ==='[object Function]'?true :false;
+    },
+
+    find:function(arr,fn){
+
+    }
+}
